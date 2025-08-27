44aca65dc7be8d8b8e0ce218c2059e69b67e1fa8
selimreza777 <selimrezacse@gmail.com>
2025-08-28 01:43:00 +0600

    cash withdraw segment done

--- home.js
+++ home.js
@@ -1,4 +1,9 @@
+// -----add money btn features star heree----------
+
 // console.log('home.js connected')
+// pin validation check bcz amader database nai so function er bire rekhe dechi 
+
+const validPin = 1234;
 document.getElementById("add-money-btn").addEventListener("click", function (e) {
   // console.log('click')
   e.preventDefault();
@@ -28,5 +33,32 @@ document.getElementById("add-money-btn").addEventListener("click", function (e) 
   // console.log(totalNewAvailableBlance)
 })
 
-// pin validation check bcz amader database nai so function er bire rekhe dechi 
-const validPin = 1234; 
+// cash out features start here 
+
+document.getElementById("withdraw-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  // console.log("withdraw button click")
+  const amount = parseInt(document.getElementById("withdraw-amount").value)
+  const availableBlance = parseInt(document.getElementById("available-blance").innerText);
+  // console.log(amount, availableBlance);
+  const newTotalAvailableBalance = availableBlance - amount
+  // console.log(newTotalAvailableBalance)
+  document.getElementById("available-blance").innerText = newTotalAvailableBalance;
+})
+
+
+
+
+
+
+
+// toggling features 
+document.getElementById("add-button").addEventListener("click", function () {
+  document.getElementById("cash-out-parent").style.display = "none";
+  document.getElementById("add-money-parent").style.display = "block"
+})
+
+document.getElementById("cash-out-button").addEventListener("click", function () {
+  document.getElementById("cash-out-parent").style.display = "block";
+  document.getElementById("add-money-parent").style.display = "none"
+})
